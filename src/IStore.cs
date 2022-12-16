c637e867fa2bbc6199cc37ed22003dd0c40b6091
SDuo <sduo@vip.qq.com>
2022-12-16 15:01:27 +0800

    更新接口定义

--- src/IStore.cs
+++ src/IStore.cs
@@ -4,12 +4,12 @@ namespace Hestia.MQ.Abstractions
 {
     public interface IStore
     {
-        public Task<int?> BeginPublishAsync(Message message);
+        public Task<int> BeginPublishAsync(Message message);
 
-        public Task<int?> EndPublishAsync(Message message);
+        public Task<int> EndPublishAsync(Message message);
 
-        public Task<int?> BeginConsumeAsync(Message message);
+        public Task<int> BeginConsumeAsync(Message message);
 
-        public Task<int?> EndConsumeAsync(Message message);
+        public Task<int> EndConsumeAsync(Message message);
     }
 }
