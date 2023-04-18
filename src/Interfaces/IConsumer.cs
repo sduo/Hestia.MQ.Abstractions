494fa1d5abe00f5d119da8b6e300314769cf5dca
SDuo <sduo@vip.qq.com>
2023-04-19 00:51:34 +0800

    add Name Property

--- src/Interfaces/IConsumer.cs
+++ src/Interfaces/IConsumer.cs
@@ -4,6 +4,8 @@ namespace Hestia.MQ.Abstractions
 {
     public interface IConsumer
     {
+        string Name { get; }
+
         [Obsolete("Consume(Func<Message, long> callback)")]
         public void Consume(Action<Message> callback);
         [Obsolete("Consume(Func<Message, long> callback)")]
--- src/Interfaces/IProducer.cs
+++ src/Interfaces/IProducer.cs
@@ -2,6 +2,7 @@
 {
     public interface IProducer
     {
+        string Name { get; }
         string Publish(Message message);
     }
 }
